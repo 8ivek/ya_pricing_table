28f87d25e70735ed814ea16794d01dba3aa73995
8ivek <bivek_j@yahoo.com>
2021-07-17 02:17:25 +0545

    create tables function added

--- includes/class-yapt-activator.php
+++ includes/class-yapt-activator.php
@@ -20,17 +20,105 @@
  * @subpackage Yapt/includes
  * @author     bvk <bivek_j@yahoo.com>
  */
-class Yapt_Activator {
+class Yapt_Activator
+{
 
-	/**
-	 * Short Description. (use period)
-	 *
-	 * Long Description.
-	 *
-	 * @since    1.0.0
-	 */
-	public static function activate() {
+    /**
+     * Short Description. (use period)
+     *
+     * Long Description.
+     *
+     * @since    1.0.0
+     */
+    public static function activate()
+    {
+        global $wpdb;
+        if (function_exists('is_multisite') && is_multisite()) {
+            // check if it is a network activation - if so, run the activation function for each blog id
+            $old_blog = $wpdb->blogid;
+            // Get all blog ids
+            $blogids = $wpdb->get_col("SELECT `blog_id` FROM $wpdb->blogs");
+            foreach ($blogids as $blog_id) {
+                switch_to_blog($blog_id);
+                // create tables
+                (new Yapt_Activator)->create_tables();
+            }
+            switch_to_blog($old_blog);
+        } else {
+            // create tables
+            (new Yapt_Activator)->create_tables();
+        }
+    }
 
-	}
+    /**
+     * Create tables when activating plugin
+     */
+    private function create_tables()
+    {
+        require_once(ABSPATH . 'wp-admin/includes/upgrade.php');
 
+        global $wpdb;
+        // ya_pricing_tables
+        $table_name = $wpdb->prefix . 'ya_pricing_tables';
+        $charset_collate = $wpdb->get_charset_collate();
+        if ($wpdb->get_var("show tables like '{$table_name}'") != $table_name) {
+            $sql = "CREATE TABLE " . $table_name . " (
+             `id` INT(11) NOT NULL AUTO_INCREMENT,
+             `name` VARCHAR(255) NOT NULL,
+             `template_id` INT(11) NOT NULL,
+			 `created_at` DATETIME NOT NULL,
+			 `updated_at` DATETIME NOT NULL,
+			  PRIMARY KEY id (id)
+		)$charset_collate;";
+            dbDelta($sql);
+        }
+
+        // ya_templates
+        $table_name = $wpdb->prefix . 'ya_templates';
+        $charset_collate = $wpdb->get_charset_collate();
+        if ($wpdb->get_var("show tables like '{$table_name}'") != $table_name) {
+            $sql = "CREATE TABLE " . $table_name . " (
+             `id` INT(11) NOT NULL AUTO_INCREMENT,
+             `template_name` VARCHAR(255) NOT NULL,
+             `style` VARCHAR(255) NOT NULL,
+             `js` VARCHAR(255) NOT NULL,
+			 `created_at` DATETIME NOT NULL,
+			 `updated_at` DATETIME NOT NULL,
+			  PRIMARY KEY id (id)
+		)$charset_collate;";
+            dbDelta($sql);
+        }
+
+        // ya_columns
+        $table_name = $wpdb->prefix . 'ya_columns';
+        $charset_collate = $wpdb->get_charset_collate();
+        if ($wpdb->get_var("show tables like '{$table_name}'") != $table_name) {
+            $sql = "CREATE TABLE " . $table_name . " (
+             `id` INT(11) NOT NULL AUTO_INCREMENT,
+             `table_id` VARCHAR(255) NOT NULL,
+             `price_text` VARCHAR(255) NOT NULL,
+             `ctoa_btn_text` VARCHAR(255) NOT NULL,/** ctoa => call to action */
+             `ctoa_btn_link` VARCHAR(255) NOT NULL,
+			 `created_at` DATETIME NOT NULL,
+			 `updated_at` DATETIME NOT NULL,
+			  PRIMARY KEY id (id)
+		)$charset_collate;";
+            dbDelta($sql);
+        }
+
+        // ya_features
+        $table_name = $wpdb->prefix . 'ya_features';
+        $charset_collate = $wpdb->get_charset_collate();
+        if ($wpdb->get_var("show tables like '{$table_name}'") != $table_name) {
+            $sql = "CREATE TABLE " . $table_name . " (
+             `id` INT(11) NOT NULL AUTO_INCREMENT,
+             `feature_text` VARCHAR(255) NOT NULL,
+             `is_set` ENUM('0', '1') NOT NULL DEFAULT '1',
+			 `created_at` DATETIME NOT NULL,
+			 `updated_at` DATETIME NOT NULL,
+			  PRIMARY KEY id (id)
+		)$charset_collate;";
+            dbDelta($sql);
+        }
+    }
 }
